c764986208158d6c06d168145396542c91800916
yemiwebby <yemiwebby@gmail.com>
2023-06-22 23:18:31 +0100

    initial commit

---
+++ __tests__/todos.test.js
@@ -0,0 +1,12 @@
+const supertest = require("supertest");
+const app = require("../app");
+const request = supertest(app);
+
+describe("GET /todos", () => {
+  it("should retrieve and list todos", async () => {
+    const response = await request.get("/todos");
+
+    expect(response.body).toBeInstanceOf(Object);
+    expect(response.body.length).toBe(3);
+  });
+});
